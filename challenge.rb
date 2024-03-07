4a619b7a4c759eeb8238dc08026983d8b1d18733
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-03-07 11:56:11 -0800

    updated text game off to lunch

--- challenge.rb
+++ challenge.rb
@@ -7,32 +7,7 @@
 #   puts "\n"
 #   gets
 
-
-## Plane-Crash Survival 
-# puts "Enter you full name"
-# user_name = gets.chomp
-
-
-# "MAYDAY MAYDAY this is #{user_name} Pilot of LEARN Academey flight 3724. We are going down! I repeat we are going down".each_char do |char|
-# putc char
-# sleep 0.05
-# end
-# puts "\n"
-
-# puts "#{user_name} choose your path: 'Wake up'  or 'Follow the light'"
-# user_response = gets.chomp.downcase
-
-# def wake_up(user_name)
-#   puts "#{user_name} WAKE UP! WAKE UP. its me Trish we have crashed and we are stranded on this remote Island".each_char do |char|
-#   putc char
-#   sleep 0.05
-# end
-# puts "\n"
-# end
-
-# if user_response == "wake up"
-#   wake_up(user_name)
-# end
+## Plane-Crash Survival
 
 puts "Enter your full name"
 user_name = gets.chomp
@@ -56,5 +31,30 @@ def wake_up(user_name)
 end
 
 if user_response == "wake up" || user_response == "follow the light"
-  puts wake_up(user_name)
+  puts wake_up(user_name) 
+else user_response == nil
+  puts wake_up(user_name) 
+end
+
+puts "#{user_name} what should we do? 'search the wreckage' or 'explore the wilderness'?"
+user_response2 = gets.chomp.downcase
+
+if user_response2 == "search the wreckage"
+  puts "#{user_name} thats a good idea. lets see what we find."
+elsif user_response2 == "explore the wilderness"
+  puts "#{user_name} theres loud roars coming from the forrest maybe theres something in the wreckage that can help us"
+else user_response2 == nil 
+  puts "i dont know about that. lets just search the wreckage first."
+end
+
+puts "#{user_name} we found a knife, some rope, flare gun, matches, first aid kit, a compass, and a fishing pole"
+puts "Now that we are supplied what should we do next? 'use compass' or use our 'use flare gun'"
+user_response3 = gets.chomp.downcase
+
+if user_response3 == "use compass" 
+  puts "Thats a good idea #{user_name} we should head north to find a way off this rock."
+elsif user_response3 == "use flare gun"
+  puts "we might need that later #{user_name} where it'll be mopre helpful."
+else user_response3 == nil 
+  puts "Are you okay? lets just start by using the compass."
 end
